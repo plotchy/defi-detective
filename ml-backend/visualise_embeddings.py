a561b3ddc8daedfb10624ae1eb94d6b0c52dcbf4
Aleksander Misztal <aleksandermisztal@gmail.com>
2023-02-27 14:51:38 -0700

    pca progress

--- ml-backend/visualise_embeddings.py
+++ ml-backend/visualise_embeddings.py
@@ -1,64 +1,46 @@
 import os
 import json
+import random
 import numpy as np
 from scipy import linalg as LA
+from matplotlib import pyplot as MPL
 
-
-def PCA(data, dims_rescaled_data=2):
-    """
-    returns: data transformed in 2 dims/columns + regenerated original data
-    pass in: data as 2D NumPy array
-    """
-    m, n = data.shape
-    # mean center the data
+def PCA(data):
     data -= data.mean(axis=0)
-    # calculate the covariance matrix
     R = np.cov(data, rowvar=False)
-    # calculate eigenvectors & eigenvalues of the covariance matrix
-    # use 'eigh' rather than 'eig' since R is symmetric, 
-    # the performance gain is substantial
     evals, evecs = LA.eigh(R)
-    # sort eigenvalue in decreasing order
     idx = np.argsort(evals)[::-1]
     evecs = evecs[:,idx]
-    # sort eigenvectors according to same index
-    evals = evals[idx]
-    # select the first n eigenvectors (n is desired dimension
-    # of rescaled data array, or dims_rescaled_data)
-    evecs = evecs[:, :dims_rescaled_data]
-    # carry out the transformation on the data using eigenvectors
-    # and return the re-scaled data, eigenvalues, and eigenvectors
-    return np.dot(evecs.T, data.T).T, evals, evecs
+    evecs = evecs[:, :2]
+    data_resc = np.dot(evecs.T, data.T).T
+    return data_resc[:, 0], data_resc[:, 1]
 
-def plot_pca(data_resc, labels):
-    from matplotlib import pyplot as MPL
+def plot_pca(xs, ys, labels):
     clr1 =  '#2026B2'
     fig = MPL.figure()
     ax1 = fig.add_subplot(111)
-    ax1.plot(data_resc[:, 0], data_resc[:, 1], '.', mfc=clr1, mec=clr1)
+    ax1.plot(xs, ys, '.', mfc=clr1, mec=clr1)
     for i, txt in enumerate(labels):
-        if i % 10 == 0:
-            ax1.annotate(txt, (data_resc[i, 0], data_resc[i, 1]))
+        if txt == 'uniswap-v2' or txt == 'olympus-dao' or txt == 'sushiswap' or random.random() < 0.05:
+            ax1.annotate(txt, (xs[i], ys[i]))
     MPL.savefig("pca.png")
 
 def load_data(path):
     files = os.listdir(path)
-    data = {}
+    data = []
     for file in files:
         with open(path + file) as f:
-            data[file] = json.loads(f.read())["data"][0]['embedding']
-    
-    return data
+            data.append(json.loads(f.read())["data"][0]['embedding'])
+    names = [file[:-9] for file in files]
+    return names, data
 
-data = load_data('./embeddings/')
-names = [name for name, emb in data.items()]
-data_matrix = np.array([emb for name, emb in data.items()])
-data_resc, _eigenvals, _eigenvecs = PCA(data_matrix)
+names, data = load_data('./embeddings/')
+data = np.array(data)
+xs, ys = PCA(data)
 
-plot_pca(data_resc, names)
+plot_pca(xs, ys, names)
 
-
-data_json = {name[:-9]: list(data) for name, data in zip(names, data_resc)}
 with open ('pca.json', 'w') as f:
+    data_json = {name: list(data) for name, data in zip(names, zip(xs, ys))}
     f.write(json.dumps(data_json))
 
