bc6a0dc908226fa30c1075c4e72c9d6cf5ccf60b
Darryl Yeo <dev@darryl-yeo.com>
2023-02-27 16:41:39 -0800

    Install Layercake; add ScatterWebgl components

---
+++ frontend/src/components/LayerCake/DotPlot.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import { LayerCake, Svg, WebGL, Html } from 'layercake';
+
+	import ScatterWebGL from './Scatter.webgl.svelte';
+	import AxisX from './AxisX.svelte';
+	import AxisY from './AxisY.svelte';
+	import QuadTree from './QuadTree.html.svelte';
+
+
+	type Datum = $$Generic<{}>
+
+	export let data: Datum[]
+
+	export const xAccessor = 'x';
+	export const yAccessor = 'y';
+
+	const r = 3;
+	const padding = 6;
+</script>
+
+
+<style>
+	/*
+		The wrapper div needs to have an explicit width and height in CSS.
+		It can also be a flexbox child or CSS grid element.
+		The point being it needs dimensions since the <LayerCake> element will
+		expand to fill it.
+	*/
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+
+	.circle {
+		position: absolute;
+		border-radius: 50%;
+		background-color: rgba(171,0, 214);
+		transform: translate(-50%, -50%);
+		pointer-events: none;
+		width: 10px;
+		height: 10px;
+	}
+</style>
+
+
+<div class="chart-container">
+	<LayerCake
+		padding={{ top: 0, right: 5, bottom: 20, left: 25 }}
+		x={xAccessor}
+		y={yAccessor}
+		xPadding={[padding, padding]}
+		yPadding={[padding, padding]}
+		data={data}
+	>
+		<Svg>
+			<AxisX/>
+			<AxisY
+				ticks={5}
+			/>
+		</Svg>
+
+		<WebGL>
+			<ScatterWebGL
+				{r}
+			/>
+		</WebGL>
+
+		<Html>
+			<QuadTree
+				let:x
+				let:y
+				let:visible
+			>
+				<div
+					class="circle"
+					style="top:{y}px;left:{x}px;display: { visible ? 'block' : 'none' };"
+				></div>
+			</QuadTree>
+		</Html>
+	</LayerCake>
+</div>
