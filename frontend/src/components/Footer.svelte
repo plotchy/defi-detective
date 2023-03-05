b4117cdcd2c9f776b0c977d9d269e32fcd34adf6
Darryl Yeo <dev@darryl-yeo.com>
2023-03-05 04:32:15 -0800

    fix twitter :(

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -8,7 +8,7 @@
 	hacked & scraped together by 
 	<a href="https://twitter.com/plotchy" target="_blank" rel="noreferrer">@plotchy</a>,
 	<a href="https://twitter.com/aleks_misztal" target="_blank" rel="noreferrer">@aleks_misztal</a> &
-	<a href="https://twitter.com/darrylyeo" target="_blank" rel="noreferrer">@darrylyeo</a>
+	<a href="https://twitter.com/darryl__yeo" target="_blank" rel="noreferrer">@darryl__yeo</a>
 	at
 	<a href="https://www.ethdenver.com/">ETHDenver 2023</a>
 </footer>
