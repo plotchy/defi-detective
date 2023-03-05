274ec8b8f0b0bbb659b89570aecd0741835e45b0
Darryl Yeo <dev@darryl-yeo.com>
2023-03-04 23:05:08 -0800

    Introduce <Context> component to expose getContext() data

---
+++ frontend/src/components/Context.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	type T = $$Generic<object>
+
+	import { getContext } from 'svelte'
+
+	export let key: string
+
+	export let context = getContext<T>(key)
+</script>
+
+
+<slot {context} />
