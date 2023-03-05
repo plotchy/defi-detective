145b9f50820922e4fab6b1ffa27611b0a87e6d9a
Aleksander Misztal <aleksandermisztal@gmail.com>
2023-03-05 01:47:52 -0700

    Fix Accept many connections

--- fetching-backend/src/bytecode_analyzer/mod.rs
+++ fetching-backend/src/bytecode_analyzer/mod.rs
@@ -82,6 +82,133 @@ pub async fn run_bytecode_analyzer(
     println!("Listening on: {}", addr);
 
     let mut streams = Vec::new();
+    let th = std::thread::spawn(|| {
+        'analyzer: loop {
+            let msg = node_msg_rx.recv().await.unwrap();
+            if !bytecode_settings.enable_existing_contract_matches && !&msg.new_creation {
+                // we are not analyzing existing contracts, and this is not a new contract, so skip
+                continue;
+            }
+            // info!("New contract from: {}", &msg.network);
+            let msg_address = format!("{:?}", msg.address);
+            let msg_network = msg.network.to_string();
+
+            // if address and network already exists in existing_matches, then skip
+            if existing_matches
+                .matches
+                .iter()
+                .any(|match_| match_.address == msg_address && match_.network == msg_network)
+            {
+                continue;
+            }
+            // if address and network already exists in new_matches, then skip
+            if new_matches
+                .matches
+                .iter()
+                .any(|match_| match_.address == msg_address && match_.network == msg_network)
+            {
+                continue;
+            }
+
+            let (event_matches, selector_matches) = retreive_matches_for_markers(&msg.bytecode);
+
+            if event_matches.is_some() || selector_matches.is_some() {
+                // add matches to abs_match_output_path
+                let events_to_add: Vec<String> = if event_matches.is_some() {
+                    let event_matches = event_matches.unwrap();
+                    let event_matches = event_matches.iter();
+                    let event_matches = event_matches
+                        .map(|index| {
+                            // match index to events position, and then get event.name
+                            events.events[index].name.as_str().to_string()
+                        })
+                        .collect();
+                    event_matches
+                } else {
+                    vec![]
+                };
+
+                let selectors_to_add: Vec<String> = if selector_matches.is_some() {
+                    let selector_matches = selector_matches.unwrap();
+                    let selector_matches = selector_matches.iter();
+                    let selector_matches = selector_matches
+                        .map(|index| {
+                            // match index to events position, and then get event.name
+                            selectors.selectors[index].name.as_str().to_string()
+                        })
+                        .collect();
+                    selector_matches
+                } else {
+                    vec![]
+                };
+
+                if msg.new_creation {
+                    new_matches.add_new_match(
+                        format!("{:?}", msg.address),
+                        msg.network.to_string(),
+                        events_to_add.clone(),
+                        selectors_to_add.clone(),
+                        Some(format!("{:?}", msg.address_from)),
+                    );
+
+                    let most_similar_contracts = get_most_similar_contracts(
+                        &similar_contracts,
+                        &events_to_add,
+                        &selectors_to_add,
+                    );
+
+                    // create a WSMessage from a NodeBytecodeMessage
+                    let ws_message = WSMessage::from_node_bytecode_message_events_fns(
+                        msg.clone(),
+                        events_to_add.clone(),
+                        selectors_to_add.clone(),
+                        most_similar_contracts,
+                    );
+                    ws_msg_cache.push(ws_message.clone());
+
+                    let ws_message = serde_json::to_string(&ws_message).unwrap();
+                    write_ws_msg_vec_to_cache(&mut ws_msg_cache, &abs_cache_path);
+                    for ws_sender in streams.iter_mut() {
+                        match ws_sender.send(Message::Text(ws_message.clone())).await {
+                            Ok(_) => (),
+                            Err(error) => {
+                                eprintln!("Error sending message: {}", error);
+                                // continue; // commented out as i switched to looping reconnects
+                                break 'analyzer;
+                            }
+                        };
+                    }
+                } else {
+                    existing_matches.add_new_match(
+                        format!("{:?}", msg.address),
+                        msg.network.to_string(),
+                        events_to_add,
+                        selectors_to_add,
+                        None,
+                    );
+                }
+
+                // write bytecode to file
+                let abs_bytecode_path = format!(
+                    "{}/{}/{}_{:?}.txt",
+                    std::env::current_dir().unwrap().to_str().unwrap(),
+                    &bytecode_settings.rel_filtered_bytecodes_path,
+                    msg.network,
+                    msg.address
+                );
+                write_bytecode_to_file(&msg.bytecode, &abs_bytecode_path);
+            }
+
+            // periodically write matches to file
+            if last_write.elapsed() > write_interval {
+                new_matches.write_to_file(&abs_new_contract_matches_path);
+                if bytecode_settings.enable_existing_contract_matches {
+                    existing_matches.write_to_file(&abs_existing_contract_matches_path);
+                }
+                last_write = Instant::now();
+            }
+        }
+    });
 
     while let Ok((stream, _addr)) = listener.accept().await {
         let ws_stream = accept_async(stream).await.expect("Failed to accept");
@@ -95,132 +222,7 @@ pub async fn run_bytecode_analyzer(
 
         streams.push(ws_sender);
     }
-
-    'analyzer: loop {
-        let msg = node_msg_rx.recv().await.unwrap();
-        if !bytecode_settings.enable_existing_contract_matches && !&msg.new_creation {
-            // we are not analyzing existing contracts, and this is not a new contract, so skip
-            continue;
-        }
-        // info!("New contract from: {}", &msg.network);
-        let msg_address = format!("{:?}", msg.address);
-        let msg_network = msg.network.to_string();
-
-        // if address and network already exists in existing_matches, then skip
-        if existing_matches
-            .matches
-            .iter()
-            .any(|match_| match_.address == msg_address && match_.network == msg_network)
-        {
-            continue;
-        }
-        // if address and network already exists in new_matches, then skip
-        if new_matches
-            .matches
-            .iter()
-            .any(|match_| match_.address == msg_address && match_.network == msg_network)
-        {
-            continue;
-        }
-
-        let (event_matches, selector_matches) = retreive_matches_for_markers(&msg.bytecode);
-
-        if event_matches.is_some() || selector_matches.is_some() {
-            // add matches to abs_match_output_path
-            let events_to_add: Vec<String> = if event_matches.is_some() {
-                let event_matches = event_matches.unwrap();
-                let event_matches = event_matches.iter();
-                let event_matches = event_matches
-                    .map(|index| {
-                        // match index to events position, and then get event.name
-                        events.events[index].name.as_str().to_string()
-                    })
-                    .collect();
-                event_matches
-            } else {
-                vec![]
-            };
-
-            let selectors_to_add: Vec<String> = if selector_matches.is_some() {
-                let selector_matches = selector_matches.unwrap();
-                let selector_matches = selector_matches.iter();
-                let selector_matches = selector_matches
-                    .map(|index| {
-                        // match index to events position, and then get event.name
-                        selectors.selectors[index].name.as_str().to_string()
-                    })
-                    .collect();
-                selector_matches
-            } else {
-                vec![]
-            };
-
-            if msg.new_creation {
-                new_matches.add_new_match(
-                    format!("{:?}", msg.address),
-                    msg.network.to_string(),
-                    events_to_add.clone(),
-                    selectors_to_add.clone(),
-                    Some(format!("{:?}", msg.address_from)),
-                );
-
-                let most_similar_contracts = get_most_similar_contracts(
-                    &similar_contracts,
-                    &events_to_add,
-                    &selectors_to_add,
-                );
-
-                // create a WSMessage from a NodeBytecodeMessage
-                let ws_message = WSMessage::from_node_bytecode_message_events_fns(
-                    msg.clone(),
-                    events_to_add.clone(),
-                    selectors_to_add.clone(),
-                    most_similar_contracts,
-                );
-                ws_msg_cache.push(ws_message.clone());
-
-                let ws_message = serde_json::to_string(&ws_message).unwrap();
-                write_ws_msg_vec_to_cache(&mut ws_msg_cache, &abs_cache_path);
-                for ws_sender in streams.iter_mut() {
-                    match ws_sender.send(Message::Text(ws_message.clone())).await {
-                        Ok(_) => (),
-                        Err(error) => {
-                            eprintln!("Error sending message: {}", error);
-                            // continue; // commented out as i switched to looping reconnects
-                            break 'analyzer;
-                        }
-                    };
-                }
-            } else {
-                existing_matches.add_new_match(
-                    format!("{:?}", msg.address),
-                    msg.network.to_string(),
-                    events_to_add,
-                    selectors_to_add,
-                    None,
-                );
-            }
-
-            // write bytecode to file
-            let abs_bytecode_path = format!(
-                "{}/{}/{}_{:?}.txt",
-                std::env::current_dir().unwrap().to_str().unwrap(),
-                &bytecode_settings.rel_filtered_bytecodes_path,
-                msg.network,
-                msg.address
-            );
-            write_bytecode_to_file(&msg.bytecode, &abs_bytecode_path);
-        }
-
-        // periodically write matches to file
-        if last_write.elapsed() > write_interval {
-            new_matches.write_to_file(&abs_new_contract_matches_path);
-            if bytecode_settings.enable_existing_contract_matches {
-                existing_matches.write_to_file(&abs_existing_contract_matches_path);
-            }
-            last_write = Instant::now();
-        }
-    }
+    th.join().unwrap();
 
     Ok(())
 }
