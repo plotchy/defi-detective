90039568bf1ba7a245d84372079cd9e85e361f79
Darryl Yeo <dev@darryl-yeo.com>
2023-03-05 02:06:07 -0800

    add credits

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <footer>
-	ETHDenver 2023
+	made by 
+	<a href="https://twitter.com/plotchy" target="_blank">@plotchy</a>,
+	<a href="https://twitter.com/aleks_misztal" target="_blank">@aleks</a> &
+	<a href="https://twitter.com/darrylyeo" target="_blank">@darrylyeo</a>
+	at
+	<a href="https://www.ethdenver.com/">ETHDenver 2023</a>
 </footer>
 
 <style>
