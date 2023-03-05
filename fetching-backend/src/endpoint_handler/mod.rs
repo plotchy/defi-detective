652c0c1230a65c03602ebb63d60368de79e72edb
plotchy <plotchy@tutanota.com>
2023-03-04 21:54:59 -0700

    cors

--- fetching-backend/src/endpoint_handler/mod.rs
+++ fetching-backend/src/endpoint_handler/mod.rs
@@ -21,13 +21,13 @@ pub struct MostSimilarContracts {
 pub async fn run_endpoint_handler() -> eyre::Result<()> {
     let cors = warp::cors()
         .allow_any_origin()
-        .allow_methods(vec!["*"])
+        // .allow_methods(vec!["*"])
         .allow_credentials(true)
         .expose_headers(vec!["*"])
         .max_age(Duration::from_secs(86400))
-        // .allow_methods(vec!["GET", "POST"])
-        .allow_headers(vec!["*"]);
-    // .allow_headers(vec!["User-Agent", "Sec-Fetch-Mode", "Referer", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Content-Type",]);
+        .allow_methods(vec!["GET", "POST", "OPTIONS"])
+        // .allow_headers(vec!["*"]);
+        .allow_headers(vec!["User-Agent", "Sec-Fetch-Mode", "Referer", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Content-Type",]);
 
 
     let similar_contracts = serde_json::from_str::<Vec<ProtocolEventsFns>>(include_str!(
@@ -143,7 +143,7 @@ pub async fn run_endpoint_handler() -> eyre::Result<()> {
     //     let contracts = get_most_similar_contracts(&contracts, events_to_add, selectors_to_add);
     // });
 
-    let routes = get_similar_contracts.or(get_contract_bytecode)
+    let routes = get_similar_contracts.with(cors.clone()).or(get_contract_bytecode)
         .with(cors);
     
 
